d82307e5ee3fd1d7b2d2e32b0539604c8c2189ca
MahmudsGit <golammahmud1314@gmail.com>
2022-04-26 13:54:42 +0600

    candidate store success

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Candidate;
 use App\Models\Job;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class ApplicationController extends Controller
 {
@@ -63,11 +64,6 @@ class ApplicationController extends Controller
             'bsc_board'=>'required',
             'bsc_grade'=>'required',
             'bsc_yaar'=>'required',
-            'others_degree_institute'=>'required',
-            'others_degree_board'=>'required',
-            'others_degree_grade'=>'required',
-            'others_degree_yaar'=>'required',
-            'experience'=>'required',
             'reg_number'=>'required',
             'reg_date'=>'required',
             'reference_1_name'=>'required',
@@ -81,6 +77,64 @@ class ApplicationController extends Controller
             'bank_branch'=>'required',
             'bank_draft_date'=>'required',
         ]);
+        $age = json_encode([
+            $request->age_day,
+            $request->age_month,
+            $request->age_year,
+        ]);
+        $parmanet_address_bangla = json_encode([
+            $request->village_bangla,
+            $request->post_office_bangla,
+            $request->thana_bangla,
+            $request->district_bangla,
+        ]);
+        $parmanet_address_english = json_encode([
+            $request->village_english,
+            $request->post_office_english,
+            $request->thana_english,
+            $request->district_english,
+        ]);
+        $ssc = json_encode([
+            $request->ssc_institute,
+            $request->ssc_board,
+            $request->ssc_grade,
+            $request->ssc_yaar,
+        ]);
+        $hsc = json_encode([
+            $request->hsc_institute,
+            $request->hsc_board,
+            $request->hsc_grade,
+            $request->hsc_yaar,
+        ]);
+        $diploma = json_encode([
+            $request->diploma_institute,
+            $request->diploma_board,
+            $request->diploma_grade,
+            $request->diploma_yaar,
+        ]);
+        $bsc = json_encode([
+            $request->bsc_institute,
+            $request->bsc_board,
+            $request->bsc_grade,
+            $request->bsc_yaar,
+        ]);
+        $others_degree = json_encode([
+            $request->others_degree_institute,
+            $request->others_degree_board,
+            $request->others_degree_grade,
+            $request->others_degree_yaar,
+        ]);
+        $reference_1 = json_encode([
+            $request->reference_1_name,
+            $request->reference_1_occupation,
+            $request->reference_1_address,
+        ]);
+        $reference_2 = json_encode([
+            $request->reference_2_name,
+            $request->reference_2_occupation,
+            $request->reference_2_address,
+        ]);
+
         $candidate = new Candidate();
             $candidate->full_name_bangla = $request->full_name_bangla;
             $candidate->full_name_english = $request->full_name_english;
@@ -89,14 +143,11 @@ class ApplicationController extends Controller
             $candidate->clo_name_english = $request->clo_name_english;
             $candidate->mother_name_bangla = $request->mother_name_bangla;
             $candidate->mother_name_english = $request->mother_name_english;
-            $candidate->birth_date = $request->birth_date;
-            $candidate->age =
-                $request->age;
+            $candidate->birth_date = Carbon::createFromFormat('m/d/Y', $request->birth_date)->format('Y/m/d');
+            $candidate->age =$age;
             $candidate->nid_number = $request->nid_number;
-            $candidate->parmanet_address_bangla =
-                $request->parmanet_address_bangla;
-            $candidate->parmanet_address_english =
-                $request->parmanet_address_english;
+            $candidate->parmanet_address_bangla =$parmanet_address_bangla;
+            $candidate->parmanet_address_english =$parmanet_address_english;
             $candidate->contact_address_bangla = $request->contact_address_bangla;
             $candidate->contact_address_english = $request->contact_address_english;
             $candidate->moblie_number = $request->moblie_number;
@@ -105,28 +156,21 @@ class ApplicationController extends Controller
             $candidate->religion = $request->religion;
             $candidate->nationality = $request->nationality;
             $candidate->employment = $request->employment;
-            $candidate->ssc =
-                $request->ssc;
-            $candidate->hsc =
-                $request->hsc;
-            $candidate->diploma =
-                $request->diploma;
-            $candidate->bsc =
-                $request->bsc;
-            $candidate->others_degree =
-                $request->others_degree;
+            $candidate->ssc = $ssc;
+            $candidate->hsc =$hsc;
+            $candidate->diploma =$diploma;
+            $candidate->bsc =$bsc;
+            $candidate->others_degree = $others_degree;
             $candidate->experience = $request->experience;
             $candidate->reg_number = $request->reg_number;
-            $candidate->reg_date = $request->reg_date;
-            $candidate->reference_1 =
-                $request->reference_1;
-            $candidate->reference_2 =
-                $request->reference_2;
+            $candidate->reg_date = Carbon::createFromFormat('m/d/Y', $request->reg_date)->format('Y/m/d');
+            $candidate->reference_1 = $reference_1;
+            $candidate->reference_2 = $reference_2;
             $candidate->bank_draft_number = $request->bank_draft_number;
             $candidate->bank_name = $request->bank_name;
             $candidate->bank_branch = $request->bank_branch;
-            $candidate->bank_draft_date = $request->bank_draft_date;
+            $candidate->bank_draft_date = Carbon::createFromFormat('m/d/Y', $request->bank_draft_date)->format('Y/m/d');
             $candidate->save();
-            return redirect()->back()->with('messege', 'success');
+            return redirect()->back();
     }
 }
